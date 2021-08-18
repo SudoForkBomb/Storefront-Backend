7809f11517e1b44a90803cd8d7eda736efdfd431
Chris Taylor <chrisrtaylor123@gmail.com>
2021-08-17 19:47:00 -0800

    feat: filled out Product and User models and stores

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,5 +1,4 @@
 import client from '../database'
-import express, { Request, Response } from 'express'
 
 export type Product = {
   id?: number
@@ -9,9 +8,13 @@ export type Product = {
 }
 
 export class ProductStore {
+  /**
+   * Returns a list of all products.
+   * @returns List of all products.
+   */
   async index(): Promise<Product[]> {
     try {
-      const sqlQuery = 'SELECT  * FROM products'
+      const sqlQuery: string = 'SELECT  * FROM products'
       const conn = await client.connect()
       const result = await conn.query(sqlQuery)
       conn.release()
@@ -22,9 +25,14 @@ export class ProductStore {
     }
   }
 
+  /**
+   * Search for a products by product id.
+   * @param id - id number to the desired product.
+   * @returns
+   */
   async show(id: number): Promise<Product> {
     try {
-      const sqlQuery = 'SELECT * FROM products WHERE id = ($1)'
+      const sqlQuery: string = 'SELECT * FROM products WHERE id = ($1)'
       const conn = await client.connect()
       const result = await conn.query(sqlQuery)
       conn.release()
@@ -36,10 +44,15 @@ export class ProductStore {
     }
   }
 
+  /**
+   * Create a new Product.
+   * @param user - Product object with desired details(name, price, category).
+   * @returns A copy of the new Product.
+   */
   async create(product: Product): Promise<Product> {
     try {
-      const sqlQuery =
-        'INSERT INTO products (name, price, category) VALUES($1, $2, $3, $4) RETURNING *'
+      const sqlQuery: string =
+        'INSERT INTO products (name, price, category) VALUES($1, $2, $3) RETURNING *'
       const conn = await client.connect()
       const result = await conn.query(sqlQuery, [
         product.name,
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,1 +1,81 @@
-import Client from '../database'
+import client from '../database'
+import bcrypt from 'bcrypt'
+
+const saltRounds = process.env.SALT_ROUNDS as string
+const pepper = process.env.BCRYPT_PASSWORD as string
+
+export type User = {
+  id?: number
+  firstName: string
+  lastName: string
+  password: string
+}
+
+export class UserStore {
+  /**
+   * Returns a list of all user accounts.
+   * @returns List of all user accounts.
+   */
+  async index(): Promise<User[]> {
+    try {
+      const sqlQuery: string = 'SELECT  * FROM users'
+
+      const conn = await client.connect()
+      const result = await conn.query(sqlQuery)
+      conn.release()
+      const usersList = result.rows
+
+      return usersList
+    } catch (error) {
+      throw new Error(`Could not get Users.\n ${error}`)
+    }
+  }
+
+  /**
+   * Search for a user account by user id.
+   * @param id - id number to the desired User account.
+   * @returns
+   */
+  async show(id: number): Promise<User> {
+    try {
+      const sqlQuery: string = 'SELECT * FROM users WHERE id = ($1)'
+
+      const conn = await client.connect()
+      const result = await conn.query(sqlQuery)
+      conn.release()
+      const user = result.rows[0]
+
+      return user
+    } catch (error) {
+      throw new Error(`Could not find User with id: ${id}.\n ${error}`)
+    }
+  }
+
+  /**
+   * Create a new user account.
+   * @param user - User object with desired details(firstName, lastName, password).
+   * @returns A copy of the new User account.
+   */
+  async create(user: User): Promise<User> {
+    try {
+      const sqlQuery: string =
+        'INSERT INTO users (first_name, last_name, password_digest) VALUES($1, $2, $3) RETURNING *'
+
+      const conn = await client.connect()
+      const hash = bcrypt.hashSync(user.password + pepper, parseInt(saltRounds))
+      const result = await conn.query(sqlQuery, [
+        user.firstName,
+        user.lastName,
+        hash,
+      ])
+      conn.release()
+      const newUser = result.rows[0]
+
+      return newUser
+    } catch (error) {
+      throw new Error(
+        `Error creating new User(${user.firstName}, ${user.lastName}).\n ${error}`
+      )
+    }
+  }
+}
