f20787aa12366b2ad1125f8ed90cca1e65b8101d
Chris Taylor <chrisrtaylor123@gmail.com>
2021-08-27 10:19:52 -0800

    fix: added basic db endpoints to order

--- src/models/order.ts
+++ src/models/order.ts
@@ -2,30 +2,90 @@ import client from '../database'
 
 export type Order = {
   id?: number
-  userID: number
+  user_id: number
   status: string
 }
 
-const sqlQuery: string =
+const ordersByUserSqlQuery: string =
   'SELECT * FROM orders WHERE user_id = ($1) AND status = ($2)'
 
 export class OrderStore {
-  async showCurrentOrdersByUser(userID: number) {
+  async index(): Promise<Order[]> {
     try {
+      const sqlQuery: string = 'SELECT * FROM orders'
       const conn = await client.connect()
-      const activeOrders = await conn.query(sqlQuery, [userID, 'active'])
+      const result = await conn.query(sqlQuery)
       conn.release()
 
-      return activeOrders.rows
+      return result.rows
+    } catch (error) {
+      throw new Error(`Could not get Orders.\n ${error}`)
+    }
+  }
+
+  async show(id: string): Promise<Order> {
+    try {
+      const sqlQuery: string = 'SELECT * FROM orders WHERE id = ($1)'
+      const conn = await client.connect()
+      const result = await conn.query(sqlQuery, [id])
+      conn.release()
+      const order = result.rows[0]
+
+      return order
+    } catch (error) {
+      throw new Error(`Could not find Order with id: ${id}.\n ${error}`)
+    }
+  }
+
+  async create(order: Order): Promise<Order> {
+    try {
+      const sqlQuery: string =
+        'INSERT INTO orders (user_id, status) VALUES($1, $2) RETURNING *'
+      const conn = await client.connect()
+      const result = await conn.query(sqlQuery, [order.user_id, order.status])
+
+      const newOrder = result.rows[0]
+      conn.release()
+
+      return newOrder
+    } catch (error) {
+      throw new Error(
+        `Error attempting to create new Order(${order.user_id}, ${order.status}).\n ${error}`
+      )
+    }
+  }
+  /**
+   *
+   * @param user_id
+   * @returns
+   */
+  async showCurrentOrderByUser(user_id: number): Promise<Order> {
+    try {
+      const conn = await client.connect()
+      const activeOrders = await conn.query(ordersByUserSqlQuery, [
+        user_id,
+        'active',
+      ])
+      conn.release()
+
+      return activeOrders.rows[0]
     } catch (error) {
       throw new Error(`Could not get active Orders.\n ${error}`)
     }
   }
 
-  async showCompletedOrdersByUser(userID: number) {
+  /**
+   *
+   * @param user_id
+   * @returns
+   */
+  async showCompletedOrdersByUser(user_id: number): Promise<Order[]> {
     try {
       const conn = await client.connect()
-      const completeOrders = await conn.query(sqlQuery, [userID, 'complete'])
+      const completeOrders = await conn.query(ordersByUserSqlQuery, [
+        user_id,
+        'complete',
+      ])
       conn.release()
 
       return completeOrders.rows
